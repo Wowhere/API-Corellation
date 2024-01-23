96aa74360e11634618922b0e390a2c4476199c1b
John Doe <69252550+Wowhere@users.noreply.github.com>
2024-01-23 23:47:45 +0300

    Init

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ReactiveUI;
+using System.Collections.ObjectModel;
+
+namespace api_corelation.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private ObservableCollection<string>? _Requests;
+        public ObservableCollection<string>? Requests
+        {
+            get => _Requests;
+            set => this.RaiseAndSetIfChanged(ref _Requests, value);
+        }
+        private bool _IsProxied;
+        public bool IsProxied
+        {
+            get => _IsProxied;
+            set => this.RaiseAndSetIfChanged(ref _IsProxied, value);
+        }
+    }
+}
